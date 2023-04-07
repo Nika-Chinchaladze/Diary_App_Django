b976fbd2c07284561037da9a200b70876ff71ffa
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2023-04-08 00:42:24 +0400

    next stage - add simple tests

--- class_based_app/tests.py
+++ class_based_app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
 
 # Create your tests here.
+
+
+class SimpleTests(SimpleTestCase):
+    def test_login_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_logout_page_status_code(self):
+        response = self.client.get("/logout")
+        self.assertEqual(response.status_code, 302)
+    
+    def test_register_page_status_code(self):
+        response = self.client.get("/register")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/home")
+        self.assertEqual(response.status_code, 200)
