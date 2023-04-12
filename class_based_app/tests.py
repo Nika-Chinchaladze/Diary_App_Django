a6de6eb429b70d54750b2546a9c3a0adc2485803
Nika-Chinchaladze <nika2019chinchaladze@gmail.com>
2023-04-12 22:41:55 +0400

    next stage - add url is resolved simple tests

--- class_based_app/tests.py
+++ class_based_app/tests.py
@@ -1,21 +1,53 @@
 from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+
+from . import views
 
 # Create your tests here.
 
 
-class SimpleTests(SimpleTestCase):
-    def test_login_page_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+class TestUrls(SimpleTestCase):
+    def test_login_url_is_resolved(self):
+        url = reverse("login")
+        self.assertEqual(resolve(url).func.__name__, views.LoginView.as_view().__name__)
     
-    def test_logout_page_status_code(self):
-        response = self.client.get("/logout")
-        self.assertEqual(response.status_code, 302)
+    def test_logout_url_is_resolved(self):
+        url = reverse("logout")
+        self.assertEqual(resolve(url).func.__name__, views.LogoutView.as_view().__name__)
     
-    def test_register_page_status_code(self):
-        response = self.client.get("/register")
-        self.assertEqual(response.status_code, 200)
+    def test_register_url_is_resolved(self):
+        url = reverse("register")
+        self.assertEqual(resolve(url).func.__name__, views.RegisterView.as_view().__name__)
+    
+    def test_home_url_is_resolved(self):
+        url = reverse("home")
+        self.assertEqual(resolve(url).func.__name__, views.HomeView.as_view().__name__)
+    
+    def test_new_url_is_resolved(self):
+        url = reverse("new")
+        self.assertEqual(resolve(url).func.__name__, views.AddNoteView.as_view().__name__)
+    
+    def test_user_image_url_is_resolved(self):
+        url = reverse("user-image")
+        self.assertEqual(resolve(url).func.__name__, views.AddImageView.as_view().__name__)
+    
+    def test_back_image_url_is_resolved(self):
+        url = reverse("back-image")
+        self.assertEqual(resolve(url).func.__name__, views.AddBackgroundView.as_view().__name__)
+    
+    def test_detail_url_is_resolved(self):
+        url = reverse("detail", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func.__name__, views.NoteDetailView.as_view().__name__)
+    
+    def test_update_url_is_resolved(self):
+        url = reverse("update", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func.__name__, views.NoteUpdateView.as_view().__name__)
+    
+    def test_delete_url_is_resolved(self):
+        url = reverse("delete", kwargs={"pk": 1})
+        self.assertEqual(resolve(url).func.view_class, views.NoteDeleteView)
+    
+    def test_all_url_is_resolved(self):
+        url = reverse("all")
+        self.assertEqual(resolve(url).func.__name__, views.NoteListView.as_view().__name__)
 
-    def test_home_page_status_code(self):
-        response = self.client.get("/home")
-        self.assertEqual(response.status_code, 200)
